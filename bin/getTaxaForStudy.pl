ac337ea0d5e23bb29b79a765422c2aed388be9a8
Wojtek Bazant <wbazant@vet.upenn.edu>
2021-07-30 09:12:36 -0400

    Nicer CLI

--- bin/getTaxaForStudy.pl
+++ bin/getTaxaForStudy.pl
@@ -9,19 +9,43 @@ use SraTraces;
 
 use LWP::Simple;
 
+use Getopt::Long;
 
-my ($study, $doRareTaxa) = @ARGV;
-die "Usage: $0 <study eg. PRJNA645191> <get the rare taxa rather than skip them 1/0>"
-  unless $study;
+my $study;
+my $runIdsString;
+my $runsPath;
+my $includeRareTaxa = 0;
 
-my @runs = grep {/SRR/} split "\n", get("https://www.ebi.ac.uk/ena/portal/api/filereport?accession=$study&result=read_run&fields=run_accession&format=tsv&download=true");
+sub usage {
+  die join(" ", @_, "Usage: $0 [--study studyAccession] [--runIds r1,r2,r3 ] [--runsPath runs.tsv] [--includeRareTaxa]"); 
+}
+GetOptions (
+  "study=s" => \$study,
+  "runIds=s" => \$runIdsString,
+  "runsPath=s" => \$runsPath,
+  "includeRareTaxa" => \$includeRareTaxa,
+) or die("Error in command line arguments\n");
+
+my @runs;
+if($study){
+  @runs = grep {/([DES]RR\d+)/} split "\n", get("https://www.ebi.ac.uk/ena/portal/api/filereport?accession=$study&result=read_run&fields=run_accession&format=tsv&download=true");
+} elsif ($runIdsString){
+  @runs = split ",", $runIdsString;
+} elsif ($runsPath){
+  open (my $fh, "<", $runsPath) or die "$!: $runsPath";
+  @runs = split "\n", do { local $/; <$fh>};
+} else {
+  usage("No runs specified!");
+}
+
+
 
 
 my %allAbundances;
 
 for my $run (@runs){
   my $page = get("https://trace.ncbi.nlm.nih.gov/Traces/sra/?run=$run");
-  my $taxonAbundances = taxonAbundancesFromPage($page, $doRareTaxa);
+  my $taxonAbundances = taxonAbundancesFromPage($page, $includeRareTaxa);
   for my $taxon (keys %{$taxonAbundances}){
      $allAbundances{$taxon}{$run} = $taxonAbundances->{$taxon};
   }
@@ -30,10 +54,10 @@ for my $run (@runs){
 say join "\t", "", @runs;
 
 sub fmt {
-  my ($num, $doRareTaxa) = @_;
-  my $f = $doRareTaxa ? "%d" : "%.4f";
+  my ($num, $includeRareTaxa) = @_;
+  my $f = $includeRareTaxa ? "%d" : "%.4f";
   return $num ? sprintf($f, $num) : "";
 }
 for my $taxon (sort keys %allAbundances){
-  say join "\t", $taxon, map {fmt($allAbundances{$taxon}{$_} , $doRareTaxa)} @runs;
+  say join "\t", $taxon, map {fmt($allAbundances{$taxon}{$_} , $includeRareTaxa)} @runs;
 }
